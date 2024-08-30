f9a2c33d02de830790aaedf0e4bc779f4da8a0d9
Arjon200141 <aorjon123@gmail.com>
2024-08-30 21:16:54 +0600

    Complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -10,7 +11,9 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lj0rsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+if (!process.env.DB_USER || !process.env.DB_PASS || !process.env.ACCESS_TOKEN_SECRET) {
+  throw new Error('Missing necessary environment variables');
+}
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,6 +30,87 @@ async function run() {
 
     const serviceCollection = client.db('event-elevate').collection('services');
     const bookingCollection = client.db('event-elevate').collection('booking');
+    const userCollection = client.db('event-elevate').collection('users');
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      const authorizationHeader = req.headers.authorization;
+      if (!authorizationHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+      const token = authorizationHeader.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'Unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      next();
+    };
+
+    app.get('/users/admin/:email', verifyToken,verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send({ admin: user?.role == 'admin' });
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'User already exists', insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    
+    app.get('/booking',  async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+      res.send(result);
+    });
+    
+
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = { $set: { role: 'admin' } };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find();
